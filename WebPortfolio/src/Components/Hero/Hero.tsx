2494516704848c3753de43c91a655e24dbc13cf0
Roberto <118137968+rovid19@users.noreply.github.com>
2024-10-09 23:51:30 +0200

    Implement new version of hero design

--- WebPortfolio/src/Components/Hero/Hero.tsx
+++ WebPortfolio/src/Components/Hero/Hero.tsx
@@ -1,175 +1,61 @@
 import React, { useEffect, useState } from "react";
-import { animate, motion } from "framer-motion";
+import { animate, motion, useAnimate } from "framer-motion";
 import "./HeroStyling.css";
 
 const Hero = () => {
-  const [startSecondMeshAni, setStartSecondMeshAni] = useState(false);
-  useState(false);
-  const [restartMeshAni, setRestartMeshAni] = useState(false);
-  const [loop, setLoop] = useState(false);
-
-  /*const [bottomPurpleMesh, setBottomPurpleMesh] = useState<HTMLElement | null>(
-    null
-  );
-  const [topPurpleMesh, setTopPurpleMesh] = useState<HTMLElement | null>(null);
-  const [introWrapper, setIntroWrapper] = useState<HTMLElement | null>(null);
-  const [bannerWrapper, setBannerWrapper] = useState<HTMLElement | null>(null);
-  const [lineAnimationDiv, setLineAnimationDiv] = useState<HTMLElement | null>(
-    null
-  );
-  const [header, setHeader] = useState<HTMLElement | null>(null);
-
-  const meshStaticAnimation = (): void => {
-    let counter = 1;
-
-    const startStaticMeshAnimation = setInterval(() => {
-      if (bottomPurpleMesh instanceof HTMLElement)
-        bottomPurpleMesh.style.right = `${counter}px`;
-
-      if (counter >= window.innerWidth - 200) {
-        clearInterval(startStaticMeshAnimation);
-        const exitStaticMeshAnimation = setInterval(() => {
-          if (bottomPurpleMesh instanceof HTMLElement)
-            bottomPurpleMesh.style.right = `${counter}px`;
-
-          if (counter <= -300) {
-            clearInterval(exitStaticMeshAnimation);
-            headerStartAnimation();
-            mainMeshOpacityAnimation();
-          } else {
-            counter = counter - 10;
-          }
-        }, 1);
-      } else {
-        counter = counter + 10;
-      }
-    }, 1);
- 
-  };
-
-  const mainMeshOpacityAnimation = (): void => {
-    let counter = 1;
-    const opacityMeshAnimation = setInterval(() => {
-      if (topPurpleMesh instanceof HTMLElement) {
-        topPurpleMesh.style.opacity = `${counter}%`;
-      }
-
-      if (counter === 100) {
-        clearInterval(opacityMeshAnimation);
-      }
-      counter = counter + 5;
-    }, 1);
-  };
-
-  const headerStartAnimation = (): void => {
-    let offScreenPosition = header?.getBoundingClientRect().y as number;
-
-    const headerAnimationInterval = setInterval(() => {
-      offScreenPosition = offScreenPosition + 0.5;
-
-      if (header instanceof HTMLElement) {
-        header.style.top = `${offScreenPosition}%`;
-      }
-
-      if (offScreenPosition === 0) {
-        clearInterval(headerAnimationInterval);
-      }
-    }, 5);
-  };
-
-  const linesAnimation = (): void => {
-    let counter = 1;
-    let width = 100;
-    const startLineAnimation = setInterval(() => {
-      if (lineAnimationDiv instanceof HTMLElement) {
-        lineAnimationDiv.style.width = `${width - counter}%`;
-
-        counter++;
-
-        if (width > 0) width = width - 0.5;
-        else {
-          lineAnimationDiv.style.opacity = "0";
-          clearInterval(startLineAnimation);
-        }
-      }
-    }, 10);
-  };
-
-  const startWebsiteAnimation = (): void => {
-    meshStaticAnimation();
-    linesAnimation();
-    let counter = 1;
-
-    const startInterval = setInterval(() => {
-      if (bannerWrapper && introWrapper instanceof HTMLElement) {
-        // introWrapper.style.opacity = `${counter}%`;
-        bannerWrapper.style.opacity = `${counter}%`;
-        console.log(counter);
-        if (counter >= 50) {
-          counter = counter + 5;
-          console.log(counter);
-          if (counter >= 105) {
-            clearInterval(startInterval);
-          }
-        } else {
-          counter++;
-        }
-      }
-    }, 20);
-
-    startInterval;
-  };
-
-  useEffect(() => {
-    setBottomPurpleMesh(
-      document.querySelector(".purple-hero-mesh") as HTMLElement
-    );
-    setIntroWrapper(
-      document.querySelector(".intro-div-wrapper") as HTMLElement
-    );
-    setBannerWrapper(document.querySelector(".hero-vector") as HTMLElement);
-    setLineAnimationDiv(
-      document.querySelector(".line-animation-div") as HTMLElement
-    );
-    setTopPurpleMesh(document.querySelector(".hero-mesh") as HTMLElement);
-    setHeader(document.querySelector(".header") as HTMLElement);
-  }, []);
-
-  useEffect(() => {
-    if (bottomPurpleMesh) {
-      startWebsiteAnimation();
-    }
-  }, [bottomPurpleMesh]);*/
   return (
     <section className="hero-section-main">
       <motion.img
         initial={{ x: 0, y: 700 }}
-        animate={
-          restartMeshAni || loop
-            ? { y: 700, x: 0 }
-            : startSecondMeshAni
-            ? { y: 1100, x: 200 }
-            : { x: 600, y: 900 }
-        }
-        transition={{ duration: 2 }}
-        onAnimationComplete={() => {
-          if (restartMeshAni) {
-            setRestartMeshAni(false);
-            setStartSecondMeshAni(false);
-            setLoop(true);
-          }
-          if (loop) {
-            setStartSecondMeshAni(true);
-          }
-          if (startSecondMeshAni) {
-            setRestartMeshAni(true);
-          } else {
-            setStartSecondMeshAni(true);
-          }
+        animate={{
+          x: [600, 200, 100, 600], // Adding the initial position at the end
+          y: [0, 200, 550, 0], // Adding the initial position at the end
         }}
+        transition={{ duration: 15, repeat: Infinity, ease: "easeInOut" }}
         className="light-blue-mesh"
         src="../../assets/Purple1.png"
       />
+      <motion.img
+        initial={{ x: 0, y: 700 }}
+        animate={{
+          x: [700, 700, 150, 700], // Adding the initial position at the end
+          y: [100, 220, 100, 100], // Adding the initial position at the end
+        }}
+        transition={{ duration: 15, repeat: Infinity, ease: "easeInOut" }}
+        className="light-purple-mesh"
+        src="../../assets/Purple2.png"
+      />
+
+      <div className="center-content">
+        <div className="center-div">
+          <div className="role-wrapper">
+            {" "}
+            <h3>
+              Roberto Vidović <br /> Software Developer
+            </h3>
+          </div>
+          <div className="intro-wrapper">
+            <div className="intro-heading-wrapper">
+              <h3 className="intro-subheading">TURNING CODE</h3>
+              <h1 className="intro-heading">INTO CREATIVE SOLUTIONS</h1>
+            </div>
+            <div className="cta-wrapper">
+              {" "}
+              <button className="intro-cta-button">Contact me</button>
+            </div>
+          </div>
+
+          <div className="contact-lines">
+            <div className="top-line" />
+            <div className="social-wrapper">
+              <img className="github-icon" src="../../assets/github.png" />
+              <img src="../../assets/linkedin.png" />
+              <img src="../../assets/instagram.png" />
+            </div>
+            <div className="bottom-line" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
